52feeb0c931256314398c300b7e5b22843a4b6f1
Mariano Heredia <mariano@kartjuba.com>
2018-01-25 08:42:08 -0300

    get_app_store_app_data implemented.

--- stores.py
+++ stores.py
@@ -1,7 +1,7 @@
 import subprocess
 import demjson
 import urllib
-#import requests
+import requests
 
 class GetStoreDataError(Exception):
     pass
@@ -26,6 +26,22 @@ def get_play_store_app_data(url):
         # Just assume any communication error as item not found. Could be improved to parse the error thrown:
         raise GetStoreDataItemNotFound() from err
 
+def get_app_store_app_data(url):
+    app_id = parse_store_app_url(url, "itunes.apple.com", "id")
+    err = None
+    try:
+        r = requests.get("https://itunes.apple.com/lookup?id={0}".format(app_id))
+        if r.status_code == 200:
+            return r.json()["results"][0]
+    except Exception as e:
+        # Just assume any error as item not found. Could be improved to parse the error thrown:
+        err = e
+
+    if err != None:
+        raise GetStoreDataItemNotFound() from err
+
+    raise GetStoreDataItemNotFound()
+
 def parse_store_app_url(url, expected_hostname, expected_id_param_key):
     split = urllib.parse.urlsplit(url)
 
@@ -34,9 +50,15 @@ def parse_store_app_url(url, expected_hostname, expected_id_param_key):
 
     params = urllib.parse.parse_qs(split.query)
 
-    if not expected_id_param_key in params:
+    if expected_id_param_key in params:
+        return params[expected_id_param_key][0]
+
+    return parse_app_id_from_path(split.path, expected_id_param_key)
+
+def parse_app_id_from_path(path, expected_id_param_key):
+    components = path.split(expected_id_param_key)
+    if len(components) < 2:
         raise GetStoreDataMissingIdParameterError()
 
-    app_id = params[expected_id_param_key][0]
-
-    return app_id
+    # Get the string to the right of id token and then discard any additional path after it
+    return components[1].split("/")[0]
--- test_stores.py
+++ test_stores.py
@@ -17,7 +17,27 @@ def test_get_play_store_app_data_success():
     result = stores.get_play_store_app_data("https://play.google.com/store/apps/details?id=com.android.chrome")
     assert result["title"] == 'Google Chrome: Fast & Secure'
 
-def test_get_play_store_app_data_exce():
+def test_get_play_store_app_data_not_found():
     with pytest.raises(stores.GetStoreDataItemNotFound):
         stores.get_play_store_app_data("https://play.google.com/store/apps/?id=non_existent_id")
+
+def test_get_app_store_app_data_success():
+    result = stores.get_app_store_app_data("https://itunes.apple.com/us/app/google-chrome/id535886823")
+    assert result["trackName"] == 'Google Chrome'
+
+def test_get_app_store_app_data_not_found():
+    with pytest.raises(stores.GetStoreDataItemNotFound):
+        stores.get_app_store_app_data("https://itunes.apple.com/us/app/google-chrome/idno_existent")
+
+def test_parse_app_id_from_path_last_component():
+    id = stores.parse_app_id_from_path("us/app/google-chrome/id535886823", "id")
+    assert id == "535886823"
+
+def test_parse_app_id_from_path_not_last_component():
+    id = stores.parse_app_id_from_path("us/app/google-chrome/id535886823/extra/paths", "id")
+    assert id == "535886823"
+
+def test_parse_app_id_from_path_missing():
+    with pytest.raises(stores.GetStoreDataMissingIdParameterError):
+        stores.parse_app_id_from_path("us/app/google-chrome", "id")
 
