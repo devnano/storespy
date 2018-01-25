12cf2f14636a53a1f20ef7cb00f54679a924ccce
Mariano Heredia <mariano@kartjuba.com>
2018-01-25 05:50:45 -0300

    Implementing exceptions.

--- stores.py
+++ stores.py
@@ -3,17 +3,26 @@ import demjson
 import urllib
 #import requests
 
+class StoreDataError(Exception):
+    pass
+    
+
+class StoreDataBadHostError(StoreDataError):
+    pass
 
 def retrieve_play_store_app_data(url):
-    splited = urllib.parse.urlsplit(url)
-    params = urllib.parse.parse_qs(splited.query)
-    app_id = params["id"][0]
+    app_id = parse_store_app_url(url, "play.google.com", "id")
     args = ["node", "node/scraper.js", app_id]
     result = subprocess.check_output(args).decode()
 
     return demjson.decode(result)
 
+def parse_store_app_url(url, expected_hostname, expected_id_param_key):
+    split = urllib.parse.urlsplit(url)
+    if split.hostname != expected_hostname:
+        raise StoreDataBadHostError()
 
-# return requests.get('https://api.github.com/events') #
+    params = urllib.parse.parse_qs(split.query)
+    app_id = params["id"][0]
 
-
+    return app_id
--- test_stores.py
+++ test_stores.py
@@ -1,11 +1,18 @@
 import stores
 import pytest
 
+def test_parse_store_bad_url_error():
+    with pytest.raises(stores.StoreDataBadHostError):
+        stores.parse_store_app_url("any string that's not an url", "play.google.com", "id")
+
+
+def test_parse_store_bad_host_error():
+    with pytest.raises(stores.StoreDataBadHostError):
+        stores.parse_store_app_url("http://google.com", "play.google.com", "id")
 
 def test_retrive_play_store_app_data_success():
     result = stores.retrieve_play_store_app_data("https://play.google.com/store/apps/details?id=com.android.chrome")
     assert result["title"] == 'Google Chrome: Fast & Secure'
-
 
 def test_retrive_play_store_app_data_except():
     with pytest.raises(Exception):
