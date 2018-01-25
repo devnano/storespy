a279636c3102ae6ec63d3f13c63613469a637f57
Mariano Heredia <mariano@kartjuba.com>
2018-01-24 23:30:47 -0300

    Android app data: Success and except cases implemented.

--- stores.py
+++ stores.py
@@ -1,13 +1,18 @@
 import subprocess
 import demjson
+import urllib
 #import requests
 
 
-
-
 def retrieve_play_store_app_data(url):
-   result = subprocess.check_output(["node", "node/test.js"]).decode()
-   return demjson.decode(result)
+    splited = urllib.parse.urlsplit(url)
+    params = urllib.parse.parse_qs(splited.query)
+    app_id = params["id"][0]
+    args = ["node", "node/scraper.js", app_id]
+    result = subprocess.check_output(args).decode()
+
+    return demjson.decode(result)
+
 
 # return requests.get('https://api.github.com/events') #
 
--- test_stores.py
+++ test_stores.py
@@ -1,6 +1,13 @@
 import stores
+import pytest
 
 
-def test_retrive_play_store_app_data():
+def test_retrive_play_store_app_data_success():
     result = stores.retrieve_play_store_app_data("https://play.google.com/store/apps/details?id=com.android.chrome")
     assert result["title"] == 'Google Chrome: Fast & Secure'
+
+
+def test_retrive_play_store_app_data_except():
+    with pytest.raises(Exception):
+        stores.retrieve_play_store_app_data("https://play.google.com/store/apps/")
+
