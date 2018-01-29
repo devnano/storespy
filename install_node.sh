75a22006fac0535c1e35dd5d6c34ef78b704889f
Mariano Heredia <mariano@kartjuba.com>
2018-01-28 22:23:04 -0300

    Linux dependan install node/npm script.

--- install_node.sh
+++ install_node.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
+# TODO: this assumes a debian box
 
-echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
-. ~/.bashrc
-mkdir ~/local
-mkdir ~/node-latest-install
-cd ~/node-latest-install
-curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
-./configure --prefix=~/local
-make install # ok, fine, this step probably takes more than 30 seconds...
-curl https://www.npmjs.org/install.sh | sh
+apt-get install nodejs npm
