79f49e334bc0d4ae5ff22dfb58e4fe8fc42218f4
Daniel Martinez <daniel@kartjuba.com>
2018-05-14 16:28:03 +0545

    Version Bump

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ DEPENDENCY_LINKS = [
 
 setuptools.setup(
     name='storespy',
-    version='0.0.20',
+    version='0.0.21',
     description='Get Google Play and App Store app info.',
     packages=['storespy'],
     install_requires=REQUIRED_PACKAGES,
